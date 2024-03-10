5b7696922b55f894dda394836711813fa7d9dfb8
Brian Royer <brianroyer@gmail.com>
2024-03-09 23:57:49 -0500

    Change rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,26 @@
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default {
-  input: './src/EchoChamber.ts',
+  input: "src/EchoChamber.ts",
   output: [
     {
-      file: 'dist/bundle.js',
-      format: 'umd',
-      name: 'EchoChamber',
+      file: "dist/EchoChamber.cjs.js",
+      format: "cjs",
+      sourcemap: true,
     },
     {
-      file: 'dist/bundle.min.js',
-      format: 'umd',
-      name: 'EchoChamber',
-      plugins: [terser()],
-    }
+      file: "dist/EchoChamber.esm.js",
+      format: "esm",
+      sourcemap: true,
+    },
   ],
   plugins: [
     typescript(),
+    commonjs(),
+    isProduction && terser(),
   ],
 };
